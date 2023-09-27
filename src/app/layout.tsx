4052dfe947f7688a476881aac44fac64a901f337
Ankit Khurana <ankitkhurana8255@gmail.com>
2023-09-28 00:52:57 +0530

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { ThemeProvider } from '@/components/theme-provider'
-
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Ankit Khurana',
@@ -15,6 +15,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='scroll-smooth' style={{scrollBehavior:'smooth'}}>
+      <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
+      <Script strategy='lazyOnload' id='analytics'>
+      {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <body>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
